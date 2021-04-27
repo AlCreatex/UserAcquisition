9cf89c46f1d6e2a384b354955ce2ce7a6db18744
al@createx.by <al@createx.by>
2021-04-27 15:30:52 +0300

    0.1

---
+++ UserAcquisition/UserAcquisition/UserAcquisition.h
@@ -0,0 +1,18 @@
+//
+//  UserAcquisition.h
+//  UserAcquisition
+//
+//  Created by admin on 27.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserAcquisition.
+FOUNDATION_EXPORT double UserAcquisitionVersionNumber;
+
+//! Project version string for UserAcquisition.
+FOUNDATION_EXPORT const unsigned char UserAcquisitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserAcquisition/PublicHeader.h>
+
+
